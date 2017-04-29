bab19c123b1c640eb66e9569dd32b96bbb5de195
steve-offutt <stvoffutt@gmail.com>
2017-04-29 17:55:34 -0500

    Initial commit. Added nerdtree and solarized as submodules.

---
+++ .bashrc
@@ -0,0 +1,13 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+if [ -f ~/.bash_profile ]; then
+	source ~/.bash_profile
+fi
+
+#alias ls='ls --color=auto'
+#PS1='[\u@\h \W]\$ '
---
+++ make_links.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=$PWD                    # dotfiles directory
+olddir=$dir/dotfiles_old    # old dotfiles backup directory
+files=".bashrc .bash_profile .vimrc .vim .tmux.conf"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in $olddir"
+mkdir -p $olddir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    printf "Saving old ~/$file if it exists\n"
+    if [ -f ~/$file ]
+    then
+        cp -r ~/$file $olddir
+    fi
+    printf "Creating symlink to $file in home directory.\n"
+    ln -fs $dir/$file ~/$file
+done
