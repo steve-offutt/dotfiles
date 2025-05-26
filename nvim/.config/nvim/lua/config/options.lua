e0854247a595ccb27cb3093141231e06b92f91d7
Steve Offutt <stvoffutt@gmail.com>
2025-05-26 13:17:18 -0500

    completion working in python

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Make line numbers default
+vim.opt.number = true
+vim.opt.cursorline = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+
+
+
+
+
+
+
