021249f676161ce8826df20c2f4c42ac9943867e
Steve Offutt <stvoffutt@gmail.com>
2025-11-06 21:12:22 -0600

    adding lsd

--- pkgs/apt.sh
+++ pkgs/apt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install apps
-packages=(zsh vim git build-essential stow tree curl ca-certificates tmux neovim python3-virtualenv ripgrep)
+packages=(zsh lsd vim git build-essential tree curl ca-certificates tmux python3-virtualenv ripgrep)
 sudo apt update
 echo "sudo apt install -y $packages"
 sudo apt install -y "${packages[@]}"
